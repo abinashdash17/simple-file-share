f785d53249076ae6b12ff638f6422b858cf1c89a
Abinash Dash <abinashdash17@gmail.com>
2023-04-30 11:45:48 +0530

    add option for dark multiple nw interfaces

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 Steps:
 1. Install python 3
-2. open this directory in CMD and run 
-    pip install --user -r requirements.txt
-3. copy the file 'simple-file-share.py' and paste it in the directory to be shared
+2. Open this directory in CMD and run 
+      pip install --user -r requirements.txt
+3. Copy the file 'simple-file-share.py' and paste it in the directory to be shared
 4. Open the directory in CMD and run
-    python .\simple-file-share.py
+      python .\simple-file-share.py
+5. Scan the qr code to browse all the files in the directory and download your desired file.
 
 Note:
-Based on the background color of the terminal, the default print_ascii may not work.
-So I have used the invert parameter in print_ascii funtion to generate ascii qr code for dark bg consoles.
-ref:https://github.com/lincolnloop/python-qrcode/blob/main/qrcode/main.py#L292
+1. Based on the background color of the terminal, 
+   If you are using a terminal with "white background", the default qr_code output may not work. In that case, use the below command 
+      python .\simple-file-share.py w
+2. Your device may contain more than 1 network interface. Try using a diferent value for "nw_interface_id", if the default qr code doesn't work.
 
 todo:
-get wifi ip
-https://groups.google.com/g/comp.lang.python/c/tF-SXWvBVjk?pli=1
 
 sources:
 https://pypi.org/project/qrcode/
+https://groups.google.com/g/comp.lang.python/c/tF-SXWvBVjk?pli=1
+ref:https://github.com/lincolnloop/python-qrcode/blob/main/qrcode/main.py#L292
--- simple-file-share.py
+++ simple-file-share.py
@@ -4,30 +4,41 @@ import sys
 import socket
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
-sys.tracebacklimit = 0  # comment this line to debug errors/warnings
-hostname = socket.gethostname()
-ip_addr = socket.gethostbyname(hostname)
 
-url = "http://" + ip_addr + ":8000"
+def get_ip_addr(num):
+    # hostname = socket.gethostname()
+    # ip_addr = socket.gethostbyname(hostname)
+    addrs = socket.getaddrinfo(socket.gethostname(), None)
+    ip_addr = []
+    for addr in addrs:
+        ip = addr[4][0]
+        if ip.startswith("192"):
+            ip_addr.append(ip)
+    print("Network interfaces:" + str(len(ip_addr)))
+    print("Using Network interface :" + str(num))
+    try:
+        return ip_addr[num - 1]
+    except:
+        print("invalid network interface, can't print qr code")
+        sys.exit(1)
+
+
+nw_interface_id = 2
+change_bg_color = False
+if len(sys.argv) > 1:
+    change_bg_color = True
+sys.tracebacklimit = 0  # comment this line to debug errors/warnings
 print()
+url = "http://" + get_ip_addr(nw_interface_id) + ":8000"
 print("Visit the below link or use the qr: ")
 print(url)
 print("Use the 2nd QR code, if the first one doesn't work")
 
 
-# generate qr code in light bg
-qr1 = qrcode.QRCode(version=3)
-qr1.add_data(url)
+qr = qrcode.QRCode(version=2)
+qr.add_data(url)
 f = io.StringIO()
-qr1.print_ascii(out=f, invert=True)
-f.seek(0)
-print(f.read())
-
-# generate qr code in light bg
-qr2 = qrcode.QRCode(version=3)
-qr2.add_data(url)
-f = io.StringIO()
-qr2.print_ascii(out=f)
+qr.print_ascii(out=f, invert=not change_bg_color)
 f.seek(0)
 print(f.read())
 
